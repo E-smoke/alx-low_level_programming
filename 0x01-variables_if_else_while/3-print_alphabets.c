b0e2779b5c022fa2b035dc9eaa550a6b051ca905
Elijah <eachala5@gmail.com>
2023-09-10 15:52:44 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main( void )
+{
+int i;
+for(i=97;i<123;++i)
+{
+putchar(i);
+}
+for(i=65;i<91;++i)
+{
+putchar(i);
+}
+putchar('\n');
+return ( 0 );
+}
