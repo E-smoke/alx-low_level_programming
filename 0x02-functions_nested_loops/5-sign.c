8ec972248334118d88a6635a9c7035a2be36d6d3
Elijah <eachala5@gmail.com>
2023-09-13 06:59:36 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+int print_sign(int n)
+{
+if((-1*n)<n)
+{
+_putchar('+');
+return 1;
+}
+
+if((-1*n)>n)
+{
+_putchar('-');
+return -1;
+}
+
+if((-1*n)==n)
+{
+_putchar('0');
+return 1;
+}
