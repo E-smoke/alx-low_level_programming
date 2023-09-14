0eff75f503d959d748818fc3f516e537f155e164
Elijah <eachala5@gmail.com>
2023-09-13 22:38:03 -0700

    102

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+int main()
+{
+long long int i,temp;
+long long int f1=1;
+long long int f2=1;
+temp=f2;
+long long sum=0;
+for(f2=1; f2<=4000000; f2=f2+f1)
+{
+temp=f2;
+f1=temp;
+if((f2%2)==0)
+{
+sum=sum+f2;
+}
+}
+printf("%lld", sum);
+return 0;
+}
 
