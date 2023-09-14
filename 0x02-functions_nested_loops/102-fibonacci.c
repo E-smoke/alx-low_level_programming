a1714239d01821f566384dc4716a8d748e8ecdd6
Elijah <eachala5@gmail.com>
2023-09-13 22:05:27 -0700

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+//#include <string.h>
+int main()
+{
+int i,temp;
+int f0=0;
+int f1=1;
+printf("%d, ", f1);
+for(i=2; i<=50; ++i)
+{
+temp=f1;;
+f1=f1+f0;
+f0=temp;
+if(i!=50)
+{
+printf("%d, ", f1);
+}
+if(i==50)
+{
+printf("%d", f1);
+}
+}
+putchar('\n');
+return 0;
+}
+
