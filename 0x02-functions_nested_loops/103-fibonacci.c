b45b8c7f2bfd502b942827116154c5698b6cbd4f
Elijah <eachala5@gmail.com>
2023-09-13 22:55:25 -0700

    102

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,12 +6,12 @@ int temp;
 int f1=1;
 int f2=1;
 int sum;
-temp=f2;
+temp=f1;
 sum=0;
-for(f2=1; f2<=4000000; f2=f2+f1)
+for(f2=1; f2<=4000000; f2=f2+temp)
 {
-temp=f2;
-f1=temp;
+temp=f1;
+f1=f2;
 if((f2%2)==0)
 {
 sum=sum+f2;
@@ -20,3 +20,4 @@ sum=sum+f2;
 printf("%d", sum);
 return 0;
 }
+
