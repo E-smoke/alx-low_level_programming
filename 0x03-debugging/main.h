14f692b8ab9b7bab241838773347ed890bbbe533
Elijah <eachala5@gmail.com>
2023-09-15 14:26:34 -0700

    2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-/**
-* positive_or_negative - prints if integer is positive or negative
-* @i: the number to be tested
-* Return: 0
-*/
 
 void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
