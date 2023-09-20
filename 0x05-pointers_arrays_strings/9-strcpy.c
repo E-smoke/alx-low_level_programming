534738670a3a0e0e7460bcb25ea2b3acfab1a3c5
Elijah <eachala5@gmail.com>
2023-09-19 20:24:36 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copy content of one string to another
+ * @dest: destination
+ * @src: source
+ * return: returns string
+ */
+char *_strcpy(char *dest, char *src)
+{
+int n=0;
+while (src[n] != '\0')
+{
+dest[n] = src[n];
+++n;
+}
+dest[n] = src[n];
+return (dest);
+}
