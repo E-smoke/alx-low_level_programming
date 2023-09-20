0e73ad01548397e14aa849b5616f86662820f979
Elijah <eachala5@gmail.com>
2023-09-20 13:59:09 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concantenate
+ * @dest: destination
+ * @src: source
+ * @n:takes a number
+ * Return: returns dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int c = 0;
+while (dest[c] != '\0')
+{
+++c;
+}
+for (i = 0; i < n; ++i)
+{
+dest[c + i] = src[i];
+}
+dest[c + i] = '\0';
+return (dest);
+}
+
