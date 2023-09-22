d5812fac2585e00abc730cb9bffb77697c837f6e
Elijah <eachala5@gmail.com>
2023-09-22 06:29:18 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,23 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
+int d = 0;
 for (i = 0; i < n; ++i)
+{
+if (src[i] == '\0')
+{
+dest[i] = '\0';
+d = i;
+}
+else if (d < i)
+{
+dest[i] = '\0';
+}
+else
 {
 dest[i] = src[i];
 }
-dest[i] =  '\0';
+}
 return (dest);
 }
 
