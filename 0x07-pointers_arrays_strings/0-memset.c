c80d2f02d86c3e62a400805dca3c15744a4872e4
Elijah <eachala5@gmail.com>
2023-09-25 09:43:33 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - the function
+ * @s: first para
+ * @b: second para
+ * @n: third para
+ * Return: returns a pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0; i < n; ++i)
+{
+s[i] = 'b';
+}
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <unistd.h>
+
+int _putchar(char c);
+
+char *_memset(char *s, char b, unsigned int n);
+
+
