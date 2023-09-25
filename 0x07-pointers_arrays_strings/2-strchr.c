cadc4aa99eab5c4dd2f4354d97845a0ceb55df6c
Elijah <eachala5@gmail.com>
2023-09-25 10:43:45 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - the funct
+ * @s: first para
+ * @c: second para
+ * Return: indicator
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+return (NULL);
+}
