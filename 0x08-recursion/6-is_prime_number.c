1f8667a0799b0a3b75ba48baeec04f0153aaab36
Elijah <eachala5@gmail.com>
2023-09-27 19:18:02 +0000

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,33 @@
 #include "main.h"
 /**
+ * loop - funct
+ * @i: para
+ * @n: para
+ * Return: bool
+ */
+int loop(int i, int n)
+{
+if (n == 2)
+{
+return (1);
+}
+if (i >= n)
+{
+return (1);
+}
+if ((n % i) == 0)
+{
+return(0);
+}
+return (loop((i + 1),n));
+}
+/**
  * is_prime_number - funct
  * @n: para
  * Return: bool
  */
 int is_prime_number(int n)
 {
-int i;
-if (n == 2)
-{
-return (1);
-}
-for (i = 2; i < n; ++i)
-{
-if ((n % i) == 0)
-{
-return (0);
-}
-}
-return (1);
+int i = 2;
+return (loop(i,n));
 }
