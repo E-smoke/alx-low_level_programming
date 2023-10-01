0c697f9dbc9a904b590ba6f39759405f357d88e5
Elijah <eachala5@gmail.com>
2023-10-01 16:18:37 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: 1p
+ * @argv: 2p
+ * Return: 0 and 1 if argument contains non integer string
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int mult = 1;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 0; i < argc; ++i)
+{
+if (atoi(argv[i]) == 0)
+{
+return (1);
+}
+}
+for (i = 0; i < argc; ++i)
+{
+mult = (mult *atoi(argv[i]));
+}
+printf("%d\n", mult);
+return (0);
+}
