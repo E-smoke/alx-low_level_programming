227de4f0a5ece674822cc12a424e59feea853e99
Elijah <eachala5@gmail.com>
2023-10-01 05:58:34 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: first para
+ * @argv: second para
+ * Return: returns an integer
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int c = 0;
+while (argv[0][c] != '\0')
+{
+++c;
+}
+for (i = 0; i < c; ++i)
+{
+_putchar(argv[0][i]);
+}
+_putchar('\n');
+return (0);
+}
