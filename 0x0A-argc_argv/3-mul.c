8b181f62d52b204648471ecb8147e2ea94746614
Elijah <eachala5@gmail.com>
2023-10-01 15:08:09 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: 1p
+ * @argv: 2p
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+if (argc == 3)
+{
+printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+}
+return (0);
+}
