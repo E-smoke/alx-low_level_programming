8e3ae17e22d180e05c978117f49861fb277286f3
Elijah <eachala5@gmail.com>
2023-10-01 06:44:02 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: 1p
+ * @argv: 2p
+ * Return: return 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+_putchar(argc + 48);
+_putchar('\n');
+return (0);
+}
