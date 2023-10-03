366a6c56d384d937bb636a0e77ac114cf36d0313
Elijah <eachala5@gmail.com>
2023-10-03 11:00:06 -0700

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * create_array - the funct
+ * @size: 1p
+ * @c: 2p
+ * Return: returns a void pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+int i;
+char *ptr;
+ptr = (char *)malloc(size);
+for (i = 0; i < size; ++i)
+{
+*(ptr + i) = c;
+}
+return (ptr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_INCLUDED
+#define MAIN_INCLUDED
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+char *create_array(unsigned int size, char c);
+
+#endif
