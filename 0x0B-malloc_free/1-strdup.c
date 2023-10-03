69f73e698b91a34587c576c7af7735d5c8ac26bb
Elijah <eachala5@gmail.com>
2023-10-03 11:47:55 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,16 +14,16 @@ while (str[c] != '\0')
 {
 ++c;
 }
-if (c == 0)
+if (str == NULL)
 {
 return (NULL);
 }
-ptr = (char *)malloc(c *sizeof(char));
+ptr = (char *)malloc((c + 1) * sizeof(char));
 if (ptr == NULL)
 {
 return (NULL);
 }
-for (i = 0; i < c; ++i)
+for (i = 0; i <= c; ++i)
 {
 *(ptr + i) = str[i];
 }
