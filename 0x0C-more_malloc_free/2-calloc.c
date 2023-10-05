97454c85b04416b5a922f15912388f1d84d6ea3f
Elijah <eachala5@gmail.com>
2023-10-05 15:01:11 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,5 +13,13 @@ if ((nmemb == 0) || (size == 0))
 return (0);
 }
 ptr = malloc(nmemb * size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < nmemb; ++i)
+{
+ptr[i] = 0;
+}
 return (ptr);
 }
