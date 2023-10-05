d0ae74e7f3dab8074e6ca07cd0acbfae14b058ef
Elijah <eachala5@gmail.com>
2023-10-05 15:22:40 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,18 +9,20 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
 void *ptr;
+char *ptr1;
 if ((nmemb == 0) || (size == 0))
 {
 return (0);
 }
 ptr = malloc(nmemb * size);
+ptr1 = (char *)ptr;
 if (ptr == NULL)
 {
 return (NULL);
 }
 for (i = 0; i < nmemb; ++i)
 {
-ptr[i] = 0;
+ptr1[i] = 0;
 }
 return (ptr);
 }
