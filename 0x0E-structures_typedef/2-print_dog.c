b03913c819b543403277010567ee25025dcf788e
Elijah <eachala5@gmail.com>
2023-10-10 22:29:20 -0700

    3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_dog - funct
+ * @d: struct
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+if (name == NULL)
+{
+print("name: (nil)");
+}
+if (owner == NULL)
+{
+print("owner: (nil)");
+}
+if (age == 0)
+{
+print("age: (nil)");
+}
+}
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,5 +1,6 @@
 #ifndef m
 #define m
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
