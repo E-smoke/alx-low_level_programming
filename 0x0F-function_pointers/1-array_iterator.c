6cd43074420ee6aa318c7c067db83d86cf17f0b8
Elijah <eachala5@gmail.com>
2023-10-11 14:30:00 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - funct
+ * @array: 1p
+ * @size: 2p
+ * @action: 3p
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if ((array == NULL) || (size <= 0) || (action == NULL))
+{
+return;
+}
+for (i = 0; i < siz; ++i)
+{
+action(array[i]);
+}
+}
