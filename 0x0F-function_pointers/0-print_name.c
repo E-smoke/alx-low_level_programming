445c7ceb5a6851aa6662d33e695f89a0db849c8a
Elijah <eachala5@gmail.com>
2023-10-11 13:49:03 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_name - funct
+ * @name: 1p
+ * @f: 2p
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,6 @@
+#ifndef pro
+#define pro
+#include <unistd.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
