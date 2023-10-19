67c64f7dd61e68930f112e4431be292f262f8979
Elijah <eachala5@gmail.com>
2023-10-19 12:10:26 -0700

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - prints the elements of a liked list
+ * @h: root ptr
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+unsigned int n;
+const list_t *current;
+n = 0;
+for (current = h; current != NULL; current = current -> next)
+{
+if (current -> str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", current -> len, current -> str);
+}
+++n;
+}
+return (n);
+}
