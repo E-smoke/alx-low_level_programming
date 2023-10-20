858ba268ba0fdfe65619bcb11a05d97322e7357d
Elijah <eachala5@gmail.com>
2023-10-19 18:28:01 -0700

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * free_list - free
+ * @head: a ptr
+ */
+void free_list(list_t *head)
+{
+list_t *current;
+current = head;
+if (head == NULL)
+{
+return;
+}
+else
+{
+while(current != head)
+{
+current = head;
+while (current->next != NULL)
+{
+current = current->next;
+}
+free(current);
+current = NULL;
+}
+free(current);
+}
+}
