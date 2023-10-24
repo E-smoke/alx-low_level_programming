7fd794cb0874859510ca41dca3a561641a890ae0
Elijah <eachala5@gmail.com>
2023-10-24 11:34:40 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,7 +6,6 @@
 void free_listint2(listint_t **head)
 {
 listint_t *current;
-current = *head;
 if (head == NULL)
 {
 return;
@@ -18,6 +17,7 @@ return;
 }
 else
 {
+current = *head;
 while (*head != NULL)
 {
 current = *head;
