958b519ce0c89120dfeeecfbc056a7e4c6aaa32e
Elijah <eachala5@gmail.com>
2023-10-24 03:22:53 -0700

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_listint - prints the elements of an integer
+ * @h: a pointer to the roo
+ * Return: returns an integer
+ */
+size_t print_listint(const listint_t *h)
+{
+int c;
+const listint_t *current;
+current = h;
+c = 0;
+if (current == NULL)
+{
+return (c);
+}
+while (current != NULL)
+{
+printf("%d", current->n);
+current = current->next;
+++c;
+}
+return (c);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+singly linked list in c
+
