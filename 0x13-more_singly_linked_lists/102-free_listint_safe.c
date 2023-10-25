50a4857334c1c3fb5438ad9e873443014e05641f
Elijah <eachala5@gmail.com>
2023-10-24 19:34:19 -0700

    a1

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_listint_safe - funct
+ * head: ptr
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+int c;
+const listint_t *current;
+current = head;
+c = 0;
+if (current == NULL)
+{
+return (c);
+}
+while (current != NULL || current == head)
+{
+printf("%d\n", current->n);
+current = current->next;
+++c;
+}
+return (c);
+}
