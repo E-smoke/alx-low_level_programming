325db33581ad3735d53ec0922cfe8ddb3a70d20d
Elijah <eachala5@gmail.com>
2023-10-30 12:39:48 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,17 +13,14 @@ int ind;
 char buf;
 if (filename == NULL)
 {
-return (0);
-}
+return (0); }
 if (letters == 0)
 {
-return (0);
-}
+return (0); }
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 {
-return (0);
-}
+return (0); }
 i = 1;
 while (i <= letters)
 {
@@ -31,26 +28,20 @@ ind = read(fd, &buf, 1);
 if (ind < 0)
 {
 close(fd);
-return (0);
-}
+return (0); }
 if (ind == 0)
 {
 close(fd);
-return (i - 1);
-}
+return (i - 1); }
 ind = write(1, &buf, 1);
 if (ind < 0)
 {
 close(fd);
-return (0);
-}
+return (0); }
 if (ind == 0)
 {
 close(fd);
-return (i - 1);
-}
-++i;
-}
+return (i - 1); }
+++i; }
 close(fd);
-return (letters);
-}
+return (letters); }
