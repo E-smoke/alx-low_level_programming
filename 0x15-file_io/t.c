889cf09ec3ce7b7be3e9b383f140017f986738ee
Elijah <eachala5@gmail.com>
2023-10-31 06:20:27 -0700

    2

--- 0x15-file_io/t.c
+++ 0x15-file_io/t.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include "0-read_textfile.c"
 #include "1-create_file.c"
+#include "2-append_text_to_file.c"
 int main(void)
 {
-create_file("pr.txt", "ko");
+printf("%d", append_text_to_file("pa.txt", "kofi"));
 return (0);
 }
