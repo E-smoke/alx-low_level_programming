5ac689c85e6814c8c7ff4dbf6fef236201564f35
Elijah <eachala5@gmail.com>
2023-12-07 05:52:16 -0800

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t i;
+const dlistint_t *current;
+current = h;
+i = 0;
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+++i;
+}
+return i;
+}
