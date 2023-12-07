d80c704423b124cdbc2724cf9b38f7264175fc68
Elijah <eachala5@gmail.com>
2023-12-07 06:00:45 -0800

    00

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * print_dlistint - prints all the members of a double linked list
+ * @h: fp
+ * Return: the number of elements
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t i;
@@ -11,5 +16,5 @@ printf("%d\n", current->n);
 current = current->next;
 ++i;
 }
-return i;
+return (i);
 }
