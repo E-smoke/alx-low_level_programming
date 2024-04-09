7df0fb544905f1f4c65216c9fc13ee9a2d3a23d4
Elijah <eachala5@gmail.com>
2024-04-09 16:04:24 -0700

    s3

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -24,10 +24,11 @@ if (array[index] == value)
 return (index);
 k++;
 prev = index;
-index = k * m;
+index = k *m;
 } while (index < (int)size && array[index] < value);
 printf("Value found between indexes [%d] and [%d]\n", prev, index);
-for (; prev <= index && prev < (int)size; prev++) {
+for (; prev <= index && prev < (int)size; prev++)
+{
 printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 if (array[prev] == value)
 return (prev);
